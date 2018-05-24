93ec80490099a0b9e7d35df0490a43f5c8122e62
Qing Xia <qingxia@buffalo.edu>
2018-05-24 16:54:06 -0400

    change to newparrent class as SpectrumSignalParameters

--- thz/model/thz-spectrum-signal-parameters.h
+++ thz/model/thz-spectrum-signal-parameters.h
@@ -25,10 +25,9 @@
 #define THZ_SPECTRUM_SIGNAL_PARAMETERS_H
 
 
-#include <ns3/simple-ref-count.h>
 #include <ns3/ptr.h>
 #include <ns3/nstime.h>
-
+#include "ns3/spectrum-signal-parameters.h"
 
 namespace ns3 {
 
@@ -36,23 +35,8 @@ class THzPhy;
 class SpectrumValue;
 class Packet;
 
-/**
- * This struct provides the generic signal representation to be used by
- * all wireless technologies. Any specific wireless technology is
- * allowed to define additional signal parameters by inheriting from this
- * struct and providing additional member variables. This makes sure
- * that a minimum set of parameters (in particular, the ones needed
- * for interference calculation) is
- * common across all wireless technologies, while at the same time
- * allowing each technology to have its own specific signal parameters.
- *
- * Furthermore, since the signal parameters specific of every technology inherit
- * directly from this struct, each PHY can test (by using a dynamic
- * cast) if a signal being received belongs to a given technology or not.
- *
- * \note when inheriting from this class, make sure that the assignment operator and the copy constructor work properly, making deep copies if needed.
- */
-struct THzSpectrumSignalParameters : public SimpleRefCount<THzSpectrumSignalParameters>
+
+struct THzSpectrumSignalParameters : public SpectrumSignalParameters
 {
   /**
    * default constructor
@@ -64,45 +48,22 @@ struct THzSpectrumSignalParameters : public SimpleRefCount<THzSpectrumSignalPara
    */
   virtual ~THzSpectrumSignalParameters ();
 
-  /**
-   * copy constructor
-   */
+  // inherited from SpectrumSignalParameters
+  virtual Ptr<SpectrumSignalParameters> Copy ();
+
   THzSpectrumSignalParameters (const THzSpectrumSignalParameters& p);
-
   /**
-   * make a "virtual" copy of this class, where "virtual" refers to
-   * the fact that if the actual object is a derived class of
-   * THzSpectrumSignalParameters, then the copy is also a derived class
-   * of the same type.
-   * Each class inheriting from
-   * THzSpectrumSignalParameters should override this method and use it
-   * to call the copy constructor of the derived class.
-   *
-   * \return a copy of the (possibly derived) class
-   */
-  virtual Ptr<THzSpectrumSignalParameters> Copy ();
-
-  /**
-   * The Power Spectral Density of the
-   * waveform, in linear units. The exact unit will depend on the
-   * type of transmission medium involved: W for radio communications, Pa for
-   * underwater acoustic communications. Other transmission media to
-   * be defined.
-   *
-   * \note when THzSpectrumSignalParameters is copied, only the pointer to the PSD will be copied. This is because SpectrumChannel objects normally overwrite the psd anyway, so there is no point in making a copy.
+   * The power spectral density of the transmitted signal. 
    */
   Ptr <SpectrumValue> txPsd;
 
   /**
-   * The duration of the packet transmission. It is
-   * assumed that the Power Spectral Density remains constant for the
-   * whole duration of the transmission. In other words, all waveform
-   * have a rect shape with respect to time.
+   * The duration of the packet transmission. 
    */
   Time txDuration;
 
   /**
-   * The SpectrumPhy instance that is making the transmission
+   * The Phy instance that is making the transmission
    */
   Ptr<THzPhy> txPhy;
 
@@ -110,11 +71,29 @@ struct THzSpectrumSignalParameters : public SimpleRefCount<THzSpectrumSignalPara
    * The data packet being transmitted with this signal
    */
   Ptr<Packet> packet;
+  /**
+   * The transmission power. 
+   */
   double txPower;
+  /**
+   * The number of frequency samples from the database. 
+   */
   double numberOfSamples;
+  /**
+   * The number of sub-bands from the database. 
+   */
   double numberOfSubBands;
+  /**
+   * The bandwidth of individual sub-band. 
+   */
   double subBandBandwidth;
+  /**
+   * The center frequency of the overall signal band. 
+   */
   double centerFrequency;
+  /**
+   * The total bandwidth of the signal. 
+   */
   double totalBandwidth;
 };
 
